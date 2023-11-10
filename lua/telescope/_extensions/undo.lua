43198148a0f43d90a0c77802e6038b089e49263b
Daniel Nägele <d.naegele@gridx.de>
2023-11-10 09:00:09 +0100

    feat(defaults): add additional default key maps, #38

--- lua/telescope/_extensions/undo.lua
+++ lua/telescope/_extensions/undo.lua
@@ -16,6 +16,9 @@ local config = {
       ["<cr>"] = require("telescope-undo.actions").yank_additions,
       ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
       ["<C-cr>"] = require("telescope-undo.actions").restore,
+      -- alternative defaults, for users whose terminals do questionable things with modified <cr>
+      ["<C-y>"] = require("telescope-undo.actions").yank_deletions,
+      ["<C-r>"] = require("telescope-undo.actions").restore,
     },
     n = {
       ["y"] = require("telescope-undo.actions").yank_additions,
